4b5d882c25cc84f8763c9a260a632e9950f778ae
sanyam jain <sayamjain99300@gmail.com>
2022-10-13 09:43:22 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const open = document.getElementById('bar')
+const close = document.getElementById('close')
+const nav = document.getElementById('navbar')
+
+
+if (open) {
+    open.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
